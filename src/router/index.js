90cf7c645f86d98ed73f5971784c6901d4015a8b
Kacper <kacperrymkiewicz@gmail.com>
2023-04-26 16:42:00 +0200

    import loginview

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import SignUpView from "../views/SignUpView.vue";
+import LoginView from "../views/LoginView.vue";
 
 const routes = [
   {
