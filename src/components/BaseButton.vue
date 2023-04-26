98392ba7f3b2090ea88801b80fe9411b19bc6946
adrian <adrian@MacBook-Air-adrian.local>
2023-04-26 18:16:26 +0200

    add BaseButton component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button class="btn btn-primary">
+        <span class="span-text">
+            <slot></slot>
+        </span>
+    </button>
+</template>
+
+<style lang="scss" scoped>
+@import "@/assets/styles/global.scss";
+    button {
+        background-color: $dark;
+        border-color: $dark;
+        padding: 10px 20px;
+        margin: 30px 0;
+        font-weight: 500;
+        
+        &:hover {
+            background-color: $secondary;
+            border-color: $secondary;
+        }
+    }
+</style>
