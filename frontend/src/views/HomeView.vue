4adbecdaa77411658e5062445335b65701bc7be9
Kacper <kacperrymkiewicz@gmail.com>
2023-04-27 18:32:45 +0200

    add calendar section on homepage

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -10,8 +10,8 @@
           godzinę wizyty. Nasz system potwierdzi Twoją rezerwację i przypomni Ci o
           niej przed planowanym terminem.</p>
           <div class="home-buttons mt-3 mb-5">
-            <base-button class="mx-2">Logowanie</base-button>
-            <base-button class="mx-2" style="background-color: #E6E9EC; border: 1px solid #D1D9E2; color: rgb(95, 109, 126);">Rejestracja</base-button>
+            <router-link to="/logowanie"><base-button class="mx-2">Logowanie</base-button></router-link>
+            <router-link to="/rejestracja"><base-button class="mx-2" style="background-color: #E6E9EC; border: 1px solid #D1D9E2; color: rgb(95, 109, 126);">Rejestracja</base-button></router-link>
           </div>
         </div>
       </div>
@@ -28,19 +28,110 @@
         <div class="col-md-12">
           <h2 class="mt-5">Nasi specjaliści</h2>
           <p>Umów wizytę do wybranego lekarza</p>
-          <div class="najblizsze-terminy">
+          <div class="najblizsze-terminy mt-5">
             <div class="row">
               <div class="col-md-4">
                 <div class="termin">
                   <div class="termin-doctor">
-                    <div class="termin-icon"></div>
-                    <div class="termin-title"></div>
+                    <div class="termin-icon">
+                      <img src="../assets/images/icons/doctor.png" class="img-fluid" alt="Doktor">
+                    </div>
+                    <div class="termin-title">
+                      <h4>Internista</h4>
+                    </div>
                   </div>
-                  <div class="termin-content"></div>
+                  <div class="termin-content">
+                    <div class="termin-content-daty">
+                      <div class="row">
+                        <div class="col-md-6">
+                          <base-button class="termin-button">Dzisiaj 09:00</base-button>
+                        </div>
+                        <div class="col-md-6">
+                          <base-button class="termin-button">Dzisiaj 09:30</base-button>
+                        </div>
+                        <div class="col-md-6">
+                          <base-button class="termin-button">Jutro 09:00</base-button>
+                        </div>
+                        <div class="col-md-6">
+                          <base-button class="termin-button">Jutro 09:00</base-button>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="termin-content-more-button">
+                      <base-button class="termin-button mt-2">Więcej terminów</base-button>
+                    </div>
+                  </div>
                 </div>
               </div>
-              <div class="col-md-4"></div>
-              <div class="col-md-4"></div>
+
+              <div class="col-md-4">
+                <div class="termin">
+                  <div class="termin-doctor">
+                    <div class="termin-icon">
+                      <img src="../assets/images/icons/opthalmologist.png" class="img-fluid" alt="Doktor">
+                    </div>
+                    <div class="termin-title">
+                      <h4>Okulista</h4>
+                    </div>
+                  </div>
+                  <div class="termin-content">
+                    <div class="termin-content-daty">
+                      <div class="row">
+                        <div class="col-md-6">
+                          <base-button class="termin-button">Dzisiaj 09:00</base-button>
+                        </div>
+                        <div class="col-md-6">
+                          <base-button class="termin-button">Dzisiaj 09:30</base-button>
+                        </div>
+                        <div class="col-md-6">
+                          <base-button class="termin-button">Jutro 09:00</base-button>
+                        </div>
+                        <div class="col-md-6">
+                          <base-button class="termin-button">Jutro 09:00</base-button>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="termin-content-more-button mt-2">
+                      <base-button class="termin-button">Więcej terminów</base-button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-md-4">
+                <div class="termin">
+                  <div class="termin-doctor">
+                    <div class="termin-icon">
+                      <img src="../assets/images/icons/gastroenterologist.png" class="img-fluid" alt="Doktor">
+                    </div>
+                    <div class="termin-title">
+                      <h4>Gastrolog</h4>
+                    </div>
+                  </div>
+                  <div class="termin-content">
+                    <div class="termin-content-daty">
+                      <div class="row">
+                        <div class="col-md-6">
+                          <base-button class="termin-button">Dzisiaj 09:00</base-button>
+                        </div>
+                        <div class="col-md-6">
+                          <base-button class="termin-button">Dzisiaj 09:30</base-button>
+                        </div>
+                        <div class="col-md-6">
+                          <base-button class="termin-button">Jutro 09:00</base-button>
+                        </div>
+                        <div class="col-md-6">
+                          <base-button class="termin-button">Jutro 09:00</base-button>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="termin-content-more-button">
+                      <base-button class="termin-button mt-2">Więcej terminów</base-button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
@@ -94,5 +185,37 @@ export default {
 
   section#home {
     background-color: $primary;
+  }
+
+  .termin-button {
+    background-color: #E6E9EC!important; 
+    border: 1px solid #D1D9E2!important; 
+    color: rgb(95, 109, 126)!important;
+    font-weight: 600!important;
+    width: 100%;
+    margin: 5px 0;
+  }
+
+  .termin-title h4 {
+    font-weight: 600;
+    font-size: 22px;
+    line-height: 30px;
+    text-align: center;
+    letter-spacing: -0.01em;
+    color: #5F6D7E;
+    padding-bottom: 10px;
+  }
+
+  .termin-icon img {
+    width: 80px;
+  }
+
+  .termin {
+    background: #F8F9FB;
+    border: 1px solid #D1D9E2;
+    box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+    border-radius: 5px;
+    padding: 25px;
+    margin-bottom: 35px;
   }
 </style>
