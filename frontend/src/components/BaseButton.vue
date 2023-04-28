95458a66192409d4aa95a6d684706d15d33bb863
Adrian <adrian.prusik@student.uwm.edu.pl>
2023-04-28 16:24:54 +0200

    add type prop to BaseButton component

--- frontend/src/components/BaseButton.vue
+++ frontend/src/components/BaseButton.vue
@@ -1,22 +1,54 @@
 <template>
-    <button class="base-button btn btn-primary">
+    <button :class="classes" class="base-button btn btn-primary">
         <span class="span-text">
             <slot></slot>
         </span>
     </button>
 </template>
 
+
+<script>
+export default {
+    props: ['type'],
+    computed: {
+    classes() {
+      return {
+        'button-dark': this.type === 'dark',
+        'button-light': this.type === 'light',
+      };
+    },
+  }
+}
+
+</script>
+
 <style lang="scss" scoped>
 @import "@/assets/styles/global.scss";
     .btn.btn-primary {
-        background-color: $dark;
-        border-color: $dark;
         padding: 10px 20px;
         font-weight: 500;
 
+        
+    }
+
+    .button-dark {
+        background-color: $button-dark;
+        border-color: $button-dark;
+
         &:hover, &:active {
-            background-color: $dark-hover;
-            border-color: $dark-hover;
+            background-color: $button-dark-hover;
+            border-color: $button-dark-hover;
         }
     }
+    .button-light {
+        background-color: $button-light;
+        border-color: $button-light;
+        color: $secondary;
+
+        &:hover, &:active {
+            background-color: $button-light-hover;
+            border-color: $button-light-hover;
+        }
+    }
+
 </style>
