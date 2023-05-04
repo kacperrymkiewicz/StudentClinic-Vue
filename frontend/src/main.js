b3937f31a0ad4c9b84fb0e73c6b7332e3bc6c58e
Adrian <adrian.prusik@student.uwm.edu.pl>
2023-05-04 16:23:03 +0200

    register globally BaseCard component

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,10 +8,12 @@ import store from "./store";
 
 import BaseButton from "@/components/BaseButton.vue";
 import HelloMessage from "@/components/HelloMessage.vue";
+import BaseCard from "@/components/BaseCard.vue";
 
 createApp(App)
 .use(store)
 .use(router)
 .component('base-button', BaseButton)
 .component('hello-message', HelloMessage)
+.component('base-card', BaseCard)
 .mount("#app");
