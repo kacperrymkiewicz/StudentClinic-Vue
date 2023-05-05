3c74bdba8f1aa77cf74f59a44e0fbb67f6299c16
Adrian <adrian.prusik@student.uwm.edu.pl>
2023-05-05 11:02:30 +0200

    update BaseButton component to use hasIcon prop

--- frontend/src/components/BaseButton.vue
+++ frontend/src/components/BaseButton.vue
@@ -3,32 +3,51 @@
         <span class="span-text">
             <slot></slot>
         </span>
+        <span v-if="this.hasIcon" class="span-icon"><svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.66663 6H12.5M8.33329 1L12.744 5.41074C13.0695 5.73618 13.0695 6.26382 12.744 6.58926L8.33329 11" stroke="#F8F9FB" stroke-width="1.5" stroke-linecap="round"/></svg></span>
     </button>
 </template>
 
 
 <script>
 export default {
-    props: ['type'],
-    computed: {
-    classes() {
-      return {
-        'button-dark': this.type === 'dark',
-        'button-light': this.type === 'light',
-      };
+    props: {
+        type: {
+            type: String,
+            required: true
+        },
+        hasIcon: {
+            type: Boolean,
+            required: false,
+        }
     },
-  }
+    computed: {
+        classes() {
+            return {
+                'button-dark': this.type === 'dark',
+                'button-light': this.type === 'light',
+            };
+        },
+    },
 }
 
 </script>
 
 <style lang="scss" scoped>
 @import "@/assets/styles/global.scss";
-    .btn.btn-primary {
+    button {
         padding: 10px 20px;
         font-weight: 500;
+        margin: 30px 0;
 
-        
+        .span-icon {
+            margin-left: 10px;
+            display: inline-block;
+            transition: all 0.2s ease-in-out;
+        }
+
+        &:hover .span-icon {
+            transform: translateX(5px);
+        }
     }
 
     .button-dark {
@@ -50,5 +69,4 @@ export default {
             border-color: $button-light-hover;
         }
     }
-
 </style>
