bdada1af90dd171abb97848fa5bf622649c7fd23
Adrian <adrian.prusik@student.uwm.edu.pl>
2023-05-06 16:01:23 +0200

    add PatientPrescriptions view

--- frontend/src/views/patient/PatientPrescriptions.vue
+++ frontend/src/views/patient/PatientPrescriptions.vue
@@ -18,6 +18,17 @@
             </div>
         </div>
     </section>
+    <base-modal v-bind:modalIsOpen="modalIsOpen" v-if="modalIsOpen" @close="toggleModalIsOpen">
+        <template v-slot:title>Recepta z dnia {{ this.date }}</template>
+        <template v-slot:subtitle>{{ drug_name + " " + drug_size}}</template>
+        <template v-slot:content>
+            <p><span>Odpłatność: </span> {{ this.fee }}</p>
+            <p><span>Dawkowanie: </span> {{ this.dosage }}</p>
+            <p><span>Kod recepty: </span> {{ this.receipt_code }}</p>
+            <p><span>Wystawca: </span> {{ this.issuer }}</p>
+            <p><span>Zalecenia: </span> {{ this.recommendations }}</p>
+        </template>
+    </base-modal>
 </template>
 
 <script>
@@ -26,8 +37,23 @@
             return {
                 date: new Date().toISOString().slice(0, 10),
                 time: "9:30",
-                doctor: "Lorem Ipsum"
+                doctor: "Lorem Ipsum",
+                drug_name: "Lorem Ipsum",
+                drug_size: "0.5g",
+                fee: "100%",
+                dosage: "Lorem ipsum",
+                receipt_code: "Lorem ipsum",
+                issuer: "Lorem ipsum",
+                recommendations: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam auctor.",
+                modalIsOpen: true
             }
-        }
+        },
+        methods: {
+            toggleModalIsOpen() {
+                return this.modalIsOpen = false;
+            }
+        },
     }
 </script>
+
+<style lang="scss" scoped></style>
