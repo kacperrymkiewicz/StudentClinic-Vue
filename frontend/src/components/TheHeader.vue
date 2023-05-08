0a1f400ca96f061d277740e93bf58dd50ef4b431
Adrian <adrian.prusik@student.uwm.edu.pl>
2023-05-08 20:38:59 +0200

    add drop down menu to TheHeader

--- frontend/src/components/TheHeader.vue
+++ frontend/src/components/TheHeader.vue
@@ -21,7 +21,7 @@
               <router-link class="nav-link" active-class="active-logged-in" to="/umow-wizyte">Umów wizytę</router-link>
             </li>
           </ul>
-          <div class="profile d-flex align-items-center">
+          <div class="profile d-flex align-items-center" @click="openDropDownMenu">
             <svg class="profile-icon" width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
               <rect width="40" height="40" rx="20" fill="#5F6D7E"/>
               <path d="M20 21.3334C21.8409 21.3334 23.3333 19.841 23.3333 18C23.3333 16.1591 21.8409 14.6667 20 14.6667C18.159 14.6667 16.6666 16.1591 16.6666 18C16.6666 19.841 18.159 21.3334 20 21.3334ZM20 21.3334C17.0544 21.3334 14.6666 23.1242 14.6666 25.3334M20 21.3334C22.9455 21.3334 25.3333 23.1242 25.3333 25.3334" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
@@ -33,7 +33,30 @@
             <svg class="profile-arrow" width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M11 1.33331L6.58926 5.74406C6.26382 6.0695 5.73618 6.0695 5.41074 5.74406L1 1.33331" stroke="#5F6D7E" stroke-width="1.5" stroke-linecap="round"/>
             </svg>
-
+            <div class="drop-down-menu" v-if="isDropDownMenuOpen">
+              <ul>
+                <router-link to="/profil">
+                  <li>
+                    <span>
+                      <svg width="18" height="21" viewBox="0 0 18 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1.94 5.91249L7.94 2.16249C8.58854 1.75716 9.41146 1.75716 10.06 2.16249L16.06 5.91249C16.6448 6.27797 17 6.91891 17 7.60849V13.3915C17 14.0811 16.6448 14.722 16.06 15.0875L10.06 18.8375C9.41146 19.2428 8.58854 19.2428 7.94 18.8375L1.94 15.0875C1.35524 14.722 1 14.0811 1 13.3915V7.60849C1 6.91891 1.35524 6.27797 1.94 5.91249Z" stroke="#5F6D7E" stroke-width="2" stroke-linecap="round"/>
+                      </svg>
+                    </span>
+                    Mój profil
+                  </li>
+                </router-link>
+                <a href="#">
+                  <li>
+                    <span>
+                      <svg width="18" height="21" viewBox="0 0 18 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1.94 5.91249L7.94 2.16249C8.58854 1.75716 9.41146 1.75716 10.06 2.16249L16.06 5.91249C16.6448 6.27797 17 6.91891 17 7.60849V13.3915C17 14.0811 16.6448 14.722 16.06 15.0875L10.06 18.8375C9.41146 19.2428 8.58854 19.2428 7.94 18.8375L1.94 15.0875C1.35524 14.722 1 14.0811 1 13.3915V7.60849C1 6.91891 1.35524 6.27797 1.94 5.91249Z" stroke="#5F6D7E" stroke-width="2" stroke-linecap="round"/>
+                      </svg>
+                    </span>
+                    Wyloguj
+                  </li>
+                </a>
+              </ul>
+            </div>
           </div>
         </div>
         <div class="collapse navbar-collapse" id="navbarSupportedContent" v-else>
@@ -78,10 +101,15 @@ export default {
     return {
       isLoggedIn: true,
       name: "Lorem Ipsum",
-      role: "Pacjent"
+      role: "Pacjent",
+      isDropDownMenuOpen: false,
     }
-
-  }
+  },
+  methods: {
+      openDropDownMenu(){
+        this.isDropDownMenuOpen = !this.isDropDownMenuOpen;
+      }
+    }
 };
 </script>
 
@@ -150,6 +178,9 @@ nav {
       }
 
       div.profile {
+        position: relative;
+        cursor: pointer;
+
         div {
           text-align: left;
           margin: 0 10px;
@@ -163,6 +194,45 @@ nav {
         }
         svg.profile-arrow {
           margin-left: 30px;
+        }
+
+        div.drop-down-menu {
+          background-color: $primary;
+          width: 230px;
+          position: absolute;
+          border-radius: 0px 0px 10px 10px;
+          z-index: 1;
+          bottom: -160px;
+          left: -32px;
+          border-bottom: 1px solid #D1D9E2;
+          box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.15);
+
+          ul {
+            list-style-type: none;
+            width: 100%;
+            padding: 15px 10px 15px 10px;
+            margin: 0;
+            
+            a {
+                text-decoration: none;
+                font-weight: 500;
+                color: $secondary;
+
+              li {
+                padding: 10px 0;
+                border-radius: 10px;
+                margin: 7px 0;
+
+                &:hover {
+                  background-color: $button-light;
+                }
+
+                span {
+                  padding: 12px;
+                }
+              }
+            }
+          }
         }
       }
     }
