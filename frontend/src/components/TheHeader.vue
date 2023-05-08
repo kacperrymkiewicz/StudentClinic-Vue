d9c8bb062c33bd18b7b9c41534135e2a5182d704
Adrian <adrian.prusik@student.uwm.edu.pl>
2023-05-08 15:19:15 +0200

    restructure elements in TheHeader

--- frontend/src/components/TheHeader.vue
+++ frontend/src/components/TheHeader.vue
@@ -1,13 +1,13 @@
 <template>
   <header>
     <nav class="navbar navbar-expand-md bg-body-tertiary">
-      <div class="container-fluid" v-if="isLoggedIn">
+      <div class="container-fluid">
         <router-link to="/"><img src="../assets/images/logo.png" class="nav-logo" alt="Logo"></router-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-lg-0 logged-in">
+        <div class="logged-in collapse navbar-collapse" id="navbarSupportedContent" v-if="isLoggedIn" >
+          <ul class="navbar-nav me-auto mb-lg-0 ">
             <li class="nav-item">
               <router-link class="nav-link" active-class="active-logged-in" to="/">Strona główna</router-link>
             </li>
@@ -21,14 +21,22 @@
               <router-link class="nav-link" active-class="active-logged-in" to="/umow-wizyte">Umów wizytę</router-link>
             </li>
           </ul>
+          <div class="profile d-flex align-items-center">
+            <svg class="profile-icon" width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <rect width="40" height="40" rx="20" fill="#5F6D7E"/>
+              <path d="M20 21.3334C21.8409 21.3334 23.3333 19.841 23.3333 18C23.3333 16.1591 21.8409 14.6667 20 14.6667C18.159 14.6667 16.6666 16.1591 16.6666 18C16.6666 19.841 18.159 21.3334 20 21.3334ZM20 21.3334C17.0544 21.3334 14.6666 23.1242 14.6666 25.3334M20 21.3334C22.9455 21.3334 25.3333 23.1242 25.3333 25.3334" stroke="white" stroke-width="1.5" stroke-linecap="round"/>
+            </svg>
+            <div>
+              <p> {{ name }}</p>
+              <p> {{ role }}</p>
+            </div>
+            <svg class="profile-arrow" width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M11 1.33331L6.58926 5.74406C6.26382 6.0695 5.73618 6.0695 5.41074 5.74406L1 1.33331" stroke="#5F6D7E" stroke-width="1.5" stroke-linecap="round"/>
+            </svg>
+
+          </div>
         </div>
-      </div>
-      <div class="container-fluid" v-else>
-        <router-link to="/"><img src="../assets/images/logo.png" class="nav-logo" alt="Logo"></router-link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" v-else>
           <ul class="navbar-nav me-auto mb-lg-0">
             <li class="nav-item">
               <router-link class="nav-link" active-class="active-logged-out" to="/">Strona główna</router-link>
@@ -63,13 +71,14 @@
     </nav>
   </header>
 </template>
-
 <script>
 
 export default {
   data(){
     return {
-      isLoggedIn: true
+      isLoggedIn: true,
+      name: "Lorem Ipsum",
+      role: "Pacjent"
     }
 
   }
@@ -77,22 +86,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .nav-logo {
-    width: 50px;
-    margin-right: 20px;
+.nav-logo {
+  width: 50px;
+  margin-right: 20px;
+}
+
+nav {
+  border-bottom: 1px solid #D1D9E2;
+  background-color: $primary;
+  color: $secondary;
+
+  @media (min-width: 768px) {
+    padding: 15px;
+    padding-left: 35px;
+    padding-right: 35px;
   }
 
-  nav {
-    border-bottom: 1px solid #D1D9E2;
-    background-color: $primary;
-    color: $secondary;
-
-    @media (min-width: 768px) {
-      padding: 15px;
-      padding-left: 35px;
-      padding-right: 35px;
-  }
-
+  div {
     ul.navbar-nav {
       li.nav-item {
         margin: 0 5px;
@@ -107,7 +117,22 @@ export default {
           }
         }
       }
-      &.logged-in {
+      
+    }
+
+    div.login-section {
+    align-items: center;
+    
+      .nav-link {
+        margin: 0 10px;
+        &.active {
+          font-weight: 600;
+          color: $secondary;
+        }
+      }
+    }
+    &.logged-in {
+        ul.navbar-nav {
         li.nav-item {
           &:first-child {
             margin-left: 50px;
@@ -123,20 +148,28 @@ export default {
           }
         }
       }
-    }
-  }
-  
-  div.login-section {
-    align-items: center;
-    
-    .nav-link {
-      margin: 0 10px;
-      &.active {
-        font-weight: 600;
-        color: $secondary;
+
+      div.profile {
+        div {
+          text-align: left;
+          margin: 0 10px;
+          p {
+            &:first-child {
+              font-weight: 500;
+            }
+            margin: 0;
+            font-size: 14px;
+          }
+        }
+        svg.profile-arrow {
+          margin-left: 30px;
+        }
       }
     }
   }
+}
+  
+  
 
 
 </style>
