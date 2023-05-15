4048771bf433c5ea78fcadee27cbe4c176ae115d
Adrian <adrian.prusik@student.uwm.edu.pl>
2023-05-15 21:25:11 +0200

    update eslint config

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -15,5 +15,7 @@ module.exports = {
     "prettier/prettier": ["warn",  {"endOfLine": "auto"}],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": "off"
+
   },
 };
