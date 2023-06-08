0fc053bafc7ab62a5ed596d89cf8a3dbb7173a19
Kacper <kacperrymkiewicz@gmail.com>
2023-06-08 15:19:34 +0200

    Keep values in store on refresh page

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,9 @@
 import { createStore } from "vuex";
 import authModule from "./modules/auth/index.js"
+import createPersistedState from "vuex-persistedstate";
 
 export default createStore({
+  plugins: [createPersistedState()],
   modules: {
     auth: authModule
   },
