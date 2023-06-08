d98c3c1a840553c75cba74178863c08ded11b23f
Kacper <kacperrymkiewicz@gmail.com>
2023-06-08 23:39:27 +0200

    Fix homepage margin

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -4,7 +4,7 @@
       <div class="row">
         <div class="col-md-6 offset-md-3">
           <h1>Przychodnia studencka</h1>
-          <p class="home-text">Z myślą o Twoim komforcie i wygodzie, oferujemy możliwość umówienia się
+          <p class="home-text" v-bind:style= "[user ? 'margin-bottom: 105px' : 'margin-bottom: inherit']">Z myślą o Twoim komforcie i wygodzie, oferujemy możliwość umówienia się
           na wizytę online. Wystarczy, że zalogujesz się na swoje konto pacjenta na
           naszej stronie internetowej, a następnie wybierzesz dogodny termin i
           godzinę wizyty. Nasz system potwierdzi Twoją rezerwację i przypomni Ci o
