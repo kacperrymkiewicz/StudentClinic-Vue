197fc0e4ed31b1bde2f6bbac247aba31cbe68ab8
Adrian <adrian.prusik@student.uwm.edu.pl>
2023-06-09 11:01:51 +0200

    add validation to PatientEditProfile view

--- frontend/src/views/patient/PatientProfileEdit.vue
+++ frontend/src/views/patient/PatientProfileEdit.vue
@@ -14,47 +14,57 @@
                             <form @submit.prevent="submitForm">
                                 <div class="form-group d-flex flex-column">
                                     <label class="align-self-start" for="name">Imię</label>
-                                    <input type="text" class="form-control" id="name" v-model.trim="patient.user.firstName">
+                                    <input type="text" class="form-control" id="name" v-model.trim="state.firstName">
                                 </div>
+                                <p v-if="v$.firstName.$error"> {{ v$.firstName.$errors[0].$message }} </p>
+
                                 <div class="form-group d-flex flex-column">
-                                    <label class="align-self-start" for="surname">Nazwisko</label>
-                                    <input type="text" class="form-control" id="surname" v-model.trim="patient.user.lastName">
+                                    <label class="align-self-start" for="lastname">Nazwisko</label>
+                                    <input type="text" class="form-control" id="lastname" v-model.trim="state.lastName">
                                 </div>
+                                <p v-if="v$.lastName.$error"> {{ v$.lastName.$errors[0].$message }} </p>
+
                                 <div class="form-group d-flex flex-column">
                                     <label class="align-self-start" for="email">Adres email</label>
-                                    <input type="email" class="form-control" id="email" v-model.trim="patient.user.emailAddress">
+                                    <input type="text" class="form-control" id="email" v-model.trim="state.emailAddress">
                                 </div>
+                                <p v-if="v$.emailAddress.$error"> {{ v$.emailAddress.$errors[0].$message }} </p>
+
                                 <div class="form-group d-flex flex-column">
                                     <label class="align-self-start" for="tel">Telefon kontaktowy</label>
-                                    <input type="tel" class="form-control" id="tel" v-model.trim="patient.phoneNumber">
+                                    <input type="tel" class="form-control" id="tel" v-model.trim="state.phoneNumber">
                                 </div>
-                                <div class="form-group d-flex flex-column">
-                                    <label class="align-self-start" for="pesel">PESEL</label>
-                                    <input type="text" class="form-control" id="pesel" v-model.trim="patient.pesel">
-                                </div>
+                                <p v-if="v$.phoneNumber.$error"> {{ v$.phoneNumber.$errors[0].$message }} </p>
+
+
                                 <div class="form-group d-flex flex-column">
                                     <label class="align-self-start" for="city">Miejscowość</label>
-                                    <input type="text" class="form-control" id="city"  v-model.trim="patient.city">
+                                    <input type="text" class="form-control" id="city" v-model.trim="state.city">
                                 </div>
+                                <p v-if="v$.city.$error"> {{ v$.city.$errors[0].$message }} </p>
+
                                 <div class="form-group d-flex flex-column">
                                     <label class="align-self-start" for="postal-code">Kod pocztowy</label>
-                                    <input type="text" class="form-control" pattern="[0-9]{2}-[0-9]{3}" placeholder="00-000" v-model.trim="patient.postalCode" id="postal-code">
+                                    <input type="text" class="form-control" id="postal-code" v-model.trim="state.postalCode" placeholder="00-000">
                                 </div>
+                                <p v-if="v$.postalCode.$error"> {{ v$.postalCode.$errors[0].$message }} </p>
+
                                 <div class="form-group d-flex flex-column">
                                     <label class="align-self-start" for="street">Ulica i nr budynku</label>
-                                    <input type="text" class="form-control" id="street" v-model.trim="patient.streetAddress">
-                                </div>         
+                                    <input type="text" class="form-control" id="street" v-model.trim="state.streetAddress">
+                                </div>
+                                <p v-if="v$.streetAddress.$error"> {{ v$.streetAddress.$errors[0].$message }} </p>
 
                                 <div class="form-group d-flex flex-column">
                                     <label class="align-self-start" for="allergies">Alergie</label>
-                                    <textarea id="allergies" rows="3" v-model.trim="patient.allergies"></textarea>
+                                    <textarea id="allergies" rows="3" v-model.trim="state.allergies"></textarea>
                                 </div>
 
                                 <div class="form-group d-flex flex-column">
                                     <label class="align-self-start" for="medications">Przyjmowane leki</label>
-                                    <textarea id="medications" rows="3" v-model.trim="patient.medicationsTaken"></textarea>
+                                    <textarea id="medications" rows="3" v-model.trim="state.medicationsTaken"></textarea>
                                 </div>
-                                
+
                                 <base-button type="dark">Zapisz zmiany</base-button>
                                 <router-link to="/profil"><base-button type="light">Anuluj</base-button></router-link>
                             </form>
@@ -69,49 +79,112 @@
 
 <script>
 
-import { mapGetters } from 'vuex';
+import { mapGetters, useStore } from 'vuex';
 import axios from 'axios'
 import { useToast } from "vue-toastification";
+import { useVuelidate } from '@vuelidate/core'
+import { reactive, computed } from 'vue';
+import { required, helpers, email, alphaNum, minLength, not } from '@vuelidate/validators'
+import { useRouter } from 'vue-router'
+
 
 export default {
     setup() {
+        const store = useStore();
+        const router = useRouter();
         const toast = useToast();
-        return { toast }
+
+        const postalCodeRegex = helpers.regex(/^[0-9]{2}-[0-9]{3}/, /\d/)
+        const phoneNumberRegex = helpers.regex(/(?<!\w)(\(?(\+|00)?48\)?)?[ -]?\d{3}[ -]?\d{3}[ -]?\d{3}(?!\w)/, /\d/)
+
+        const state = reactive({
+            firstName: store.getters.patient.user.firstName,
+            lastName: store.getters.patient.user.lastName,
+            emailAddress: store.getters.patient.user.emailAddress,
+            phoneNumber: store.getters.patient.phoneNumber,
+            city: store.getters.patient.city,
+            postalCode: store.getters.patient.postalCode,
+            streetAddress: store.getters.patient.streetAddress,
+            allergies: store.getters.patient.allergies,
+            medicationsTaken: store.getters.patient.medicationsTaken,
+        })
+
+        const rules = computed(() => {
+            return {
+                firstName: { 
+                    required: helpers.withMessage('Pole nie może być puste', required),
+                    minLength: helpers.withMessage('Imię jest zbyt krótkie', minLength(2)), 
+                },
+                lastName: {
+                    required: helpers.withMessage('Pole nie może być puste', required),
+                    minLength: helpers.withMessage('Nazwisko jest zbyt krótkie', minLength(2)), 
+                },
+                emailAddress: {
+                    required: helpers.withMessage('Pole nie może być puste', required),
+                    email: helpers.withMessage("Niepoprawny adres email", email)
+                },
+                phoneNumber: {
+                    required: helpers.withMessage('Pole nie może być puste', required),
+                    phoneNumberRegex: helpers.withMessage('Niepoprawny numer telefonu', phoneNumberRegex)
+                },
+                city: {
+                    required: helpers.withMessage('Pole nie może być puste', required),
+                },
+                postalCode: {
+                    required: helpers.withMessage('Pole nie może być puste', required),
+                    postalCodeRegex: helpers.withMessage('Niepoprawny adres email', postalCodeRegex),
+                },
+                streetAddress: {
+                    required: helpers.withMessage('Pole nie może być puste', required),
+                    alphaNum: helpers.withMessage('Niepoprawna wartość', not(alphaNum)),
+                    minLength: helpers.withMessage('Podana nazwa ulicy jest zbyt krótka', minLength(3))
+                },
+            }
+        })
+    
+
+        const submitForm = async () => {
+            const isValid = await v$.value.$validate()
+            if(isValid){
+                axios
+            .put(`Patients`, {
+                id: store.getters.patient.id,
+                user: {
+                    id: store.getters.patient.id,
+                    firstName: state.firstName,
+                    lastName: state.lastName,
+                    emailAddress: state.emailAddress,
+                    accountType: "Patient",
+                },
+                pesel: store.getters.patient.pesel,
+                phoneNumber: state.phoneNumber,
+                allergies: state.allergies,
+                medicationsTaken: state.medicationsTaken,
+                city: state.city,
+                postalCode: state.postalCode,
+                streetAddress: state.streetAddress
+            })
+            .then(() => {
+                toast.success("Zmiany zostały zapisane");
+                router.replace('/profil');
+            })
+            .catch((error) => {
+                //this.toast.error("Wystąpił błąd przy edycji profilu");
+                console.log(error)
+            })
+            } else {
+                toast.error("Błędy w formularzu");
+            }
+        }
+
+        const v$ = useVuelidate(rules, state)
+
+        return { toast, state, rules, submitForm, v$ }
     },
     computed: {
         ...mapGetters(['patient', 'user'])
     },
 
-    methods: {
-        submitForm(){
-            axios
-            .put(`Patients`, {
-                id: this.patient.id,
-                user: {
-                    id: this.patient.id,
-                    firstName: this.patient.user.firstName,
-                    lastName: this.patient.user.lastName,
-                    emailAddress: this.patient.user.emailAddress,
-                    accountType: "Patient",
-                },
-                pesel: this.patient.pesel,
-                phoneNumber: this.patient.phoneNumber,
-                allergies: this.patient.allergies,
-                medicationsTaken: this.patient.medicationsTaken,
-                city: this.patient.city,
-                postalCode: this.patient.postalCode,
-                streetAddress: this.patient.streetAddress
-            })
-            .then(() => {
-                this.toast.success("Zmiany zostały zapisane");
-                this.$router.replace('/profil');
-            })
-            .catch((error) => {
-                this.toast.error("Wystąpił błąd przy edycji profilu");
-                console.log(error)
-            })
-        },  
-    }
 }
 
 </script>
